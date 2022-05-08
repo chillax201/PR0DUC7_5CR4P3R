1599d9dace1c2cfead27c6ea501bedca569ac170
chillax504 <chillax.shastra@protonmail.com>
2022-05-08 17:36:52 +0530

    com2

--- scr4p3 m41n.py
+++ scr4p3 m41n.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 prod = str(input("product:"))
 
-url1 = "https://olx.in/"+prod
+url1 = "https://www.olx.in/bengaluru_g4058803/q-"+prod+"?isSearchCall=true"
 headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0'}
-soup1 = ''
+html_T1 = requests.get(url1)
+souped1 = BeautifulSoup(html_T1, 'lxml')
