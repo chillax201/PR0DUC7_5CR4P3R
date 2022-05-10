4ebcfc561882dc8249219572357b8de43f73a1f8
chillax504 <chillax.shastra@protonmail.com>
2022-05-10 18:56:43 +0530

    com3

--- scr4p3 m41n.py
+++ scr4p3 m41n.py
@@ -13,6 +13,14 @@ souped1 = BeautifulSoup(html_T1.text, 'lxml')
 groupa = souped1.find_all('li', class_ = 'EIR5N')
 secprice = int(souped1.find_all('li', class_ = '_89yzn').split()[-1])
 prodops = []
+
 for i in groupa :
     prodops.append(str(i.find('span', class_ = '_2tW1I').text))
 print(prodops)
+
+for i in prodops :
+    url2e = "https://www.amazon.in/s?k="+i+"&crid=LPWOCPEFVKNR&sprefix=gp%2Caps%2C379&ref=nb_sb_noss_2"
+    url2 = url2e.replace(' ','+')
+    html_T2 = requests.get(url2, headers=headers)
+    souped2 = BeautifulSoup(html_T2.text, 'lxml')
+    
