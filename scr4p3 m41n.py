a952fcce2b24bff02330d692d89528b8de8fbb72
chillax :) <100826671+chillax504@users.noreply.github.com>
2022-05-18 20:34:12 +0530

    final fix as of may 18th 2022

--- scr4p3 m41n.py
+++ scr4p3 m41n.py
@@ -73,11 +73,35 @@ for i in proddict :
 
 print(prodals)
 
+for i in proddict :
+    n = proddict[i]['name']
+    url2e = "https://www.flipkart.com/search?q="+n+"&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
+    url2 = url2e.replace(' ','+')
+    html_T2 = requests.get(url2, headers=headers)
+    souped2 = BeautifulSoup(html_T2.text, 'lxml')
+    groupc = souped2.find_all('div', class_ = "_4ddWXP")
+    k = 0
+    prodalsb[i] = {'ls' : [], }
+    for j in groupc:
+        if j.find('span', class_ = '_30jeq3'):
+            prodalsb[i]['ls'].append(str(j.find('a', class_ = "s1Q9rs").text))
+            prodalsb[i][k] = int(j.find('div', class_ = '_30jeq3').text.replace(",","").replace(".","").replace("₹",""))
+            k += 1
+    print("**********************************************************************************************************************************************************************")
+    print(prodalsb)
+    print("**********************************************************************************************************************************************************************")
+    if prodalsb[i]['ls']:
+        x = check_rel(prodalsb[i]['ls'], n)
+        proddict[i]['relatedp']['prodf']['pnameb'] = prodalsb[i]['ls'][x]
+        proddict[i]['relatedp']['prodf']['pcostb'] = prodalsb[i][x]
+
+print(prodalsb)
+
 print("**********************************************************************************************************************************************************************")
 print("**********************************************************************************************************************************************************************")
 print(proddict)
 print("**********************************************************************************************************************************************************************")
 print("**********************************************************************************************************************************************************************")
 
+print("run the program for a min of 5 times with the same keyword in order for it to work at it's optimum")
 print("remember to chillax!")
-#same method will be applied for proddict[i]['relatedp']['prodf']
